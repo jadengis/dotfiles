1c48ba313ec87fb72ff2fb9b2eae78dbb6525376
John Dengis <john@deng.is>
2025-04-19 22:41:16 -0700

    fix null ls problems

--- neovim/.config/nvim/lua/config/lsp/init.lua
+++ neovim/.config/nvim/lua/config/lsp/init.lua
@@ -4,7 +4,7 @@ local M = {
   dependencies = {
     "williamboman/mason.nvim", -- Package manager for Neovim
 	  "williamboman/mason-lspconfig.nvim", -- LspConfig for mason.nvim
-	  "jose-elias-alvarez/null-ls.nvim",
+	  "nvimtools/none-ls.nvim",
     "folke/neodev.nvim",
     "utilyre/barbecue.nvim",
   }
