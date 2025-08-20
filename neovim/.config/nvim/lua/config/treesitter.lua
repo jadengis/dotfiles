843b9e56d636dcc7c88d489a703aa6f6fc204ddf
John Dengis <john@deng.is>
2025-08-19 23:34:34 -0700

    update

--- neovim/.config/nvim/lua/config/treesitter.lua
+++ neovim/.config/nvim/lua/config/treesitter.lua
@@ -4,46 +4,46 @@ local M = {
   lazy = false,
   branch = "main",
   build = ":TSUpdate",
-  init = function() 
-          local parser_installed = {
-                "javascript",
-                "javascript-react",
-                "typescript",
-                "typescript-react",
-                "html",
-                "css",
-                "elixir",
-                "heex",
-                "go",
-                "c",
-                "lua",
-                "vim",
-                "vimdoc",
-                "query",
-                "markdown_inline",
-                "markdown",
-            }
+  init = function()
+    local parser_installed = {
+      "javascript",
+      "jsx",
+      "typescript",
+      "tsx",
+      "html",
+      "css",
+      "elixir",
+      "heex",
+      "go",
+      "c",
+      "lua",
+      "vim",
+      "vimdoc",
+      "query",
+      "markdown_inline",
+      "markdown",
+    }
 
-            require("nvim-treesitter").install(parser_installed)
+    require("nvim-treesitter").install(parser_installed)
 
-            -- auto-start highlights & indentation
-            vim.api.nvim_create_autocmd("FileType", {
-                desc = "User: enable treesitter highlighting",
-                pattern = parser_installed,
-                callback = function(ctx)
-                    -- highlights
-                    local ok = pcall(vim.treesitter.start) -- errors for filetypes with no parser
-                    -- folds
-                    if ok then
-                      vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-                    end
-                    -- indent
-                    local no_indent = {}
-                    if ok and not vim.list_contains(no_indent, ctx.match) then
-                        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-                    end
-                end,
-            })
+    -- auto-start highlights & indentation
+    vim.api.nvim_create_autocmd("FileType", {
+      desc = "User: enable treesitter highlighting",
+      pattern = parser_installed,
+      callback = function(ctx)
+        -- highlights
+        local ok = pcall(vim.treesitter.start)             -- errors for filetypes with no parser
+        -- folds
+        if ok then
+          vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+        end
+        -- indent
+        local no_indent = {}
+        if ok and not vim.list_contains(no_indent, ctx.match) then
+          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end
+      end,
+    })
   end
 }
 --[[ local parser_config = require("nvim-treesitter.parsers").get_parser_configs() ]]
